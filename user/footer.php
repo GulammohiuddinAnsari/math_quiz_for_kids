89945551236231059f4215572d3e74a1430ed249
Gulam Mohiuddin Ansari <156639810+GulammohiuddinAnsari@users.noreply.github.com>
2024-09-07 15:27:27 +0530

    added some text on front page

---
+++ user/footer.php
@@ -0,0 +1,10 @@
+<head>
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+
+<div class="footer-spacer"></div>
+<footer>
+    <div class="footer-content">
+        <p>&copy; <?php echo date("Y"); ?> Maths Quiz for Kids</p>
+    </div>
+</footer>
